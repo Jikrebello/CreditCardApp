357fb53dcc4f02bbccb461f0d5590d33dba94da1
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:23:50 +0200

    Add files via upload

---
+++ CreditCardApp/frmDisplay.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace CreditCardApp
+{
+    partial class frmDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnProcess = new System.Windows.Forms.Button();
+            this.TxtProcessed = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnProcess
+            // 
+            this.BtnProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BtnProcess.Location = new System.Drawing.Point(12, 262);
+            this.BtnProcess.MinimumSize = new System.Drawing.Size(80, 40);
+            this.BtnProcess.Name = "BtnProcess";
+            this.BtnProcess.Size = new System.Drawing.Size(107, 41);
+            this.BtnProcess.TabIndex = 0;
+            this.BtnProcess.Text = "Process";
+            this.BtnProcess.UseVisualStyleBackColor = true;
+            this.BtnProcess.Click += new System.EventHandler(this.BtnProcess_Click);
+            // 
+            // TxtProcessed
+            // 
+            this.TxtProcessed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtProcessed.Location = new System.Drawing.Point(12, 12);
+            this.TxtProcessed.Multiline = true;
+            this.TxtProcessed.Name = "TxtProcessed";
+            this.TxtProcessed.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TxtProcessed.Size = new System.Drawing.Size(403, 244);
+            this.TxtProcessed.TabIndex = 1;
+            this.TxtProcessed.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 311);
+            this.Controls.Add(this.TxtProcessed);
+            this.Controls.Add(this.BtnProcess);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnProcess;
+        private System.Windows.Forms.TextBox TxtProcessed;
+    }
+}
+
---
+++ CreditCardApp/frmDisplay.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CreditCardApp
+{
+    public partial class frmDisplay : Form
+    {
+        public frmDisplay()
+        {
+            InitializeComponent();
+        }
+        
+        // Fields
+        private string applicationNumber;
+        private string salary;
+        private string rent;
+        private string yearsEmployed;
+        private string yearsResiding;
+
+        private void BtnProcess_Click(object sender, EventArgs e)
+        {
+            TxtProcessed.Text = "";
+            string contents = "";
+
+            TxtProcessed.Text = " Credit Card Application Status Report." + Environment.NewLine + Environment.NewLine;
+
+            string path = Application.StartupPath;
+            string fullPath = path + @"\Applications.txt";
+
+            StreamReader reader = new StreamReader(fullPath);
+
+            while (!reader.EndOfStream)
+            {
+                ReadCreditFile(reader);
+
+                string creditStatus = CreditCardStatus();
+
+                contents += " Application #: " + applicationNumber +
+                            "\tMonthly Salary: " + double.Parse(salary).ToString("c") + 
+                            "\tMonthly Rent: " + double.Parse(rent).ToString("c") + 
+                            "\tYears Employed: " + yearsEmployed + 
+                            "\t\tYears at Current Address: " + yearsResiding + 
+                            "\tApplication Status: " + creditStatus + " " +
+                            Environment.NewLine + 
+                            Environment.NewLine;
+            }
+
+            reader.Close();
+
+            TxtProcessed.Text += contents;
+        }
+
+        private void ReadCreditFile(StreamReader _r)
+        {
+            applicationNumber = _r.ReadLine();
+            salary = _r.ReadLine();
+            rent = _r.ReadLine();
+            yearsEmployed = _r.ReadLine();
+            yearsResiding = _r.ReadLine();
+        }
+
+        private string CreditCardStatus()
+        {
+            string status;
+    
+            // have a salary of over $25,000 per year,
+            if (double.Parse(salary) >= 25000)
+                status = "Approved.";
+            // have an annual salary of over $20,000 and pay rent of less than one quarter of a month's salary
+            else if (double.Parse(salary) >= 20000 && double.Parse(rent) <= double.Parse(salary) / 4.8)
+                status = "Approved.";
+            // have an annual salary of over $15,000 and be living at the same address for more than 5 years
+            else if (double.Parse(salary) >= 15000 && int.Parse(yearsResiding) >= 5)
+                status = "Approved.";
+            // have an annual salary of at least $10,000 and be living at the same address for at least 5 years and be employed at the same job for at least 3 years
+            else if (double.Parse(salary) >= 10000 && int.Parse(yearsResiding) >= 5 && int.Parse(yearsEmployed) >= 3)
+                status = "Approved.";
+            // All other applications are rejected.
+            else
+                status = "Rejected.";
+
+            return status;
+        }
+
+    }
+}
